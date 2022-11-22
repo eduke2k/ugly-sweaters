208309c707d2756689237683486f1c97078410a6
Edu <eduke@gmx.de>
2022-11-22 19:54:30 +0100

    initial commit (vue3)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    Header
+  </header>
+
+  <main>
+    Some content
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+
+<style>
+@import './styles/app.scss';
+</style>
